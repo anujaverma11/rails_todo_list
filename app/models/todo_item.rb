9ff6b7af63e9dd51a4cee7fe3025d48948192e35
anujaverma11 & Mark Sunghyuk Park <pair+anujaverma11.sunghyukpark@devbootcamp.com>
2015-09-10 17:06:39 -0700

    add delete, update/ complete rails routes

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,8 +1,21 @@
 class TodoItem < ActiveRecord::Base
-	before_create :default_values
-  def default_values
-    # self.completed = false
-    self.completed = false
-    true
+	# before_create :default_values
+
+ #  def default_values
+ #    self.completed = false
+ #    true
+ #  end
+
+  def complete?
+    self.completed == true
   end
+
+  def uncomplete
+    update completed: false
+  end
+
+  def complete
+    update completed: true
+  end
+
 end
--- db/migrate/20150910183321_create_todo_items.rb
+++ db/migrate/20150910183321_create_todo_items.rb
@@ -2,7 +2,7 @@ class CreateTodoItems < ActiveRecord::Migration
   def change
     create_table :todo_items do |t|
     	t.string :name
-    	t.boolean :completed
+    	t.boolean :completed, default: false
 
       t.timestamps null: false
     end
